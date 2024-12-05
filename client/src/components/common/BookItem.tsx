a56b54bf2d128686c7e8e588e91383642fe2dd29
phondani0 <ankitphondani52@gmail.com>
2024-12-06 00:02:43 +0530

    feat: add book details page

--- client/src/components/common/BookItem.tsx
+++ client/src/components/common/BookItem.tsx
@@ -1,28 +1,44 @@
 import React from "react";
 import Link from "../core/link/Link";
+import clsx from "clsx";
 
 type BookItemProps = {
     id: string;
     name: string;
     imageUrl: string;
+    className?: string;
+    showLabel?: boolean;
+    imageClassName?: string;
 };
 
-const BookItem: React.FC<BookItemProps> = ({ id, name, imageUrl }) => {
+const BookItem: React.FC<BookItemProps> = ({
+    id,
+    name,
+    imageUrl,
+    className,
+    showLabel = true,
+    imageClassName,
+}) => {
     return (
-        <div className="w-56">
+        <div className={clsx("w-48", className)}>
             <Link navigateTo={`/book?id=${id}`}>
                 <img
                     src={imageUrl}
                     alt="Book cover"
-                    className="w-full h-72 object-cover rounded-md mb-8"
+                    className={clsx(
+                        "w-full h-60 object-cover rounded-md mb-8",
+                        imageClassName
+                    )}
                     style={{
                         boxShadow: "rgb(0 0 0 / 57%) -7px 9px 14px 0px",
                     }}
                 />
             </Link>
-            <Link navigateTo={`/book?id=${id}`}>
-                <h3 className="text-sm italic">{name}</h3>
-            </Link>
+            {showLabel && (
+                <Link navigateTo={`/book?id=${id}`}>
+                    <h3 className="text-sm italic">{name}</h3>
+                </Link>
+            )}
         </div>
     );
 };
